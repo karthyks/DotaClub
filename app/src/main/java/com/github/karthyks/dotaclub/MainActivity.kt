adcc45f021ae966d240ecf6ba690f9f9f131899f
Karthik Periasami <mail2karthyk@gmail.com>
2021-02-27 22:09:07 +0700

    update compose hilt and kotlin versions

--- app/src/main/java/com/github/karthyks/dotaclub/MainActivity.kt
+++ app/src/main/java/com/github/karthyks/dotaclub/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.github.karthyks.dotaclub
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Preview
 import com.github.karthyks.dotaclub.ui.DotaClubTheme
 import dagger.hilt.android.AndroidEntryPoint
 
